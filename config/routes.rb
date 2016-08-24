bf8c4aa33e52d03d22a24ff3432feac3b44e99e9
Sergio Ocon <sergio.ocon@redhat.com>
2016-08-24 13:21:36 +0200

    updated application layout to show debug data in development

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'about', to: 'information#about'
   # Temporary root
   root to: "information#about"
+  # Resources for Rooms and Shifts
+  resources :rooms do
+    resources :shifts, shallow: true
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
