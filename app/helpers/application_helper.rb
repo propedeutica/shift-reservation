a0049713d5460d35340c661408d8a804f5218f50
Sergio Ocon <sergio.ocon@redhat.com>
2016-08-26 13:05:50 +0200

    Titleizing full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,11 @@
 #
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = t("application.name")
+    base_title = t "application.name"
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      page_title.titleize + " | " + base_title
     end
   end
 
