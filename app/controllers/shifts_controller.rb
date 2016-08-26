4bd1003e38740836b4fd4584eef9a592c6813af0
Sergio Ocon <sergio.ocon@redhat.com>
2016-08-26 15:21:52 +0200

    added shift new and create actions to controller

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -5,4 +5,18 @@ class ShiftsController < ApplicationController
       redirect_to rooms_path
     end
   end
+
+  def new
+    @shift = Shift.build
+  end
+
+  def create
+    @shift = Shift.build(offsprings_params)
+    if @offspring.save
+      flash[:success] = t "application.models.shift.shift_added"
+    else
+      flash[:danger] = t "applicatoin.models.shift.shift_not_added"
+    end
+    redirect_to @shift
+  end
 end
