fdd1fd2bde1838bdb3c15113e81e11c0832d2e9d
Sergio Ocon <sergio.ocon@redhat.com>
2016-08-27 11:58:46 +0200

    added CRUD for shifts

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -7,16 +7,52 @@ class ShiftsController < ApplicationController
   end
 
   def new
-    @shift = Shift.build
+    @room = Room.find_by_id(params[:room_id])
+    @shift = @room.shifts.build
   end
 
   def create
-    @shift = Shift.build(offsprings_params)
-    if @offspring.save
-      flash[:success] = t "application.models.shift.shift_added"
+    @room = Room.find_by_id(params[:room_id])
+    @shift = @room.shifts.build(shifts_params)
+    if @shift.save
+      flash[:success] = (t "application.models.shift.shift_added").capitalize
+      redirect_to rooms_path
     else
-      flash[:danger] = t "applicatoin.models.shift.shift_not_added"
+      flash[:danger] = (t "application.models.shift.shift_not_added").capitalize
+      render 'new'
     end
-    redirect_to @shift
+  end
+
+  def edit
+    @shift = Shift.find_by_id(params[:id])
+    @room = @shift.room
+  end
+
+  def update
+    @shift = Shift.find(params[:id])
+    @room = @shift.room
+    if @shift.update_attributes(shifts_params)
+      flash[:success] = (t "application.models.shift.shift_updated").capitalize
+      redirect_to rooms_path
+    else
+      flash[:danger] = (t "application.models.shift.shift_not_updated").capitalize
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @shift = Shift.find_by_id(params["id"])
+    if @shift && @shift.destroy
+      flash[:success] = (t "application.models.shift.shift_deleted").capitalize
+    else
+      flash[:danger] = (t "application.models.shift.shift_delete_error").capitalize
+    end
+    redirect_to rooms_path
+  end
+
+  private
+
+  def shifts_params
+    params.require(:shift).permit(:day_of_week, :start_time, :end_time, :sites_reserved)
   end
 end
