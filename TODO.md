646512fa1726fbabb90f1c49cd1342ce6499f6bc
Sergio Ocon <sergio.ocon@redhat.com>
2016-08-28 18:57:22 +0200

    added CRUD for rooms

---
+++ TODO.md
@@ -0,0 +1,4 @@
+#List of things to do:
+- Add cancel button in forms
+- Add dashboard
+- Add reports
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,54 @@ class RoomsController < ApplicationController
     @sites_available = Shift.total_sites_available
     @rooms = Room.all
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def edit
+    @room = Room.find_by_id(params[:id])
+  end
+
+  def create
+    @room = Room.new(rooms_params)
+    if @room.save
+      flash[:success] = (t 'application.models.room.room_added').capitalize
+      redirect_to room_path @room
+    else
+      flash[:error] = (t 'application.models.room.room_not_added').capitalize
+      render 'new'
+    end
+  end
+
+  def show
+    @room = Room.find_by_id(params[:id])
+  end
+
+  def update
+    @room = Room.find_by_id(params[:id])
+    if @room.update_attributes(rooms_params)
+      flash[:success] = (t "application.models.room.room_updated").capitalize
+      redirect_to room_path @room
+    else
+      flash[:danger] = (t "application.models.room.room_not_updated").capitalize
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @room = Room.find_by_id(params["id"])
+    if @room && @room.destroy
+      flash[:success] = (t "application.models.room.room_deleted").capitalize
+    else
+      flash[:danger] = (t "application.models.room.room_delete_error").capitalize
+    end
+    redirect_to rooms_path
+  end
+
+  private
+
+  def rooms_params
+    params.require(:room).permit(:name, :capacity) 
+  end
 end
