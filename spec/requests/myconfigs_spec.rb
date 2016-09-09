02e43dcc075bae33d405d8a961e783fba14a7c08
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-09 10:35:46 +0200

    adding coverage to gitignore and fix route errors

--- spec/requests/myconfigs_spec.rb
+++ spec/requests/myconfigs_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 RSpec.describe "Myconfigs", type: :request do
   describe "POST /myconfigs" do
     it "changes lock to enabled" do
-      post myconfig_global_lock_enable_path
+      post admin_myconfig_global_lock_enable_path
       expect(Myconfig.global_lock?).to be_truthy
     end
 
     it "changes lock to disabled" do
-      post myconfig_global_lock_disable_path
+      post admin_myconfig_global_lock_disable_path
       expect(Myconfig.global_lock?).to be_falsy
     end
 
     it "swithes lock" do
-      post myconfig_global_lock_enable_path
-      post myconfig_global_lock_switch_path
+      post admin_myconfig_global_lock_enable_path
+      post admin_myconfig_global_lock_switch_path
       expect(Myconfig.global_lock?).to be_falsy
+      post admin_myconfig_global_lock_switch_path
+      expect(Myconfig.global_lock?).to be_truthy
     end
   end
 end
