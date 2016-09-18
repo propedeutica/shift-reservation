2b99d1cd50b92af63d18cf2768f208039a670c8d
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-18 15:40:44 +0200

    added patternfly log in for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin && action_name == "new"
+      "patternfly_log_in"
+    else
+      "application"
+    end
+  end
 end
