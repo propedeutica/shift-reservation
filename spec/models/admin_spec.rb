4c7d541f852feb1a0ad8d155c5f40628f830c874
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-19 13:20:31 +0200

    WIP1 specs for admin

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin) { FactoryGirl.build(:admin) }
+
+  it "is valid with everything properly filled" do
+    admin.valid?
+    expect(admin).to be_valid
+  end
+
+  it "is invalid without a password" do
+    admin.password = "  "
+    admin.valid?
+    expect(admin.errors[:password]).to include "can't be blank"
+  end
+
+  it "is invalid with a password too short" do
+    admin.password = "a" * 7
+    admin.valid?
+    expect(admin.errors[:password]).to include "is too short (minimum is 8 characters)"
+  end
+
+  it "is invalid with a password too long" do
+    admin.password = "a" * 18
+    admin.valid?
+    expect(admin.errors[:password]).to include "demasiado largo"
+  end
+
 end
