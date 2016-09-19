1e4762cc49ecaa10e9d9c43927e1d10ad67ca4af
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-19 13:36:42 +0200

    WIP 2

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -23,7 +23,24 @@ RSpec.describe Admin, type: :model do
   it "is invalid with a password too long" do
     admin.password = "a" * 18
     admin.valid?
-    expect(admin.errors[:password]).to include "demasiado largo"
+    expect(admin.errors[:password]).to include "Password is too long"
   end
 
+  it "is invalid without an email" do
+    admin.email = "   "
+    admin.valid?
+    expect(admin.errors[:email]).to include "can't be blank"
+  end
+
+  it "is invalid with a too short email" do
+    admin.email = "a"
+    admin.valid?
+    expect(admin.errors[:email]).to include "Email address is too short"
+  end
+
+  it "is invalid with a too long email" do
+    admin.email = "a" * 255 + "b"
+    admin.valid?
+    expect(admin.errors[:email]).to include "is invalid"
+  end
 end
