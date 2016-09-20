83feb6610805f58bbd723d753abd3b3a884c6865
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-20 16:40:26 +0200

    added graphics to rooms for admin

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,20 @@ class Room < ApplicationRecord
   has_many  :shifts, dependent: :destroy
   validates :name, presence: true
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def total_capacity
+    shifts.count * capacity
+  end
+
+  def total_sites_available
+    tsitesavailable = 0
+    shifts.each do |x|
+      tsitesavailable += x.sites_available
+    end
+    tsitesavailable
+  end
+
+  def total_occupied
+    total_capacity - total_sites_available
+  end
 end
