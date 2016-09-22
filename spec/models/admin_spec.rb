41f0b1b83728b75a49b5d115e0ce4542fca602a1
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-22 17:32:30 +0200

    WIP

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
+  active_record_user = 'activerecord.errors.models.admin.attributes'
   let(:admin) { FactoryGirl.build(:admin) }
 
   it "is valid with everything properly filled" do
@@ -11,25 +12,25 @@ RSpec.describe Admin, type: :model do
   it "is invalid without a password" do
     admin.password = "  "
     admin.valid?
-    expect(user.errors[:password]).to include(I18n.t('password.blank', scope: active_record_user))
+    expect(admin.errors[:password]).to include(I18n.t('password.blank', scope: active_record_user))
   end
 
   it "is invalid with a password too short" do
     admin.password = "a" * 7
     admin.valid?
-    expect(user.errors[:password]).to include(I18n.t('password.too_short', count: 8, scope: active_record_user))
+    expect(admin.errors[:password]).to include(I18n.t('password.too_short', count: 8, scope: active_record_user))
   end
 
   it "is invalid with a password too long" do
     admin.password = "a" * 129
     admin.valid?
-    expect(user.errors[:password]).to include(I18n.t('password.too_long', count: 129, scope: active_record_user))
+    expect(admin.errors[:password]).to include(I18n.t('password.too_long', count: 129, scope: active_record_user))
   end
 
   it "is invalid without an email" do
     admin.email = "   "
     admin.valid?
-    expect(user.errors[:email]).to include(I18n.t('email.blank', scope: active_record_user))
+    expect(admin.errors[:email]).to include(I18n.t('email.blank', scope: active_record_user))
   end
 
   it "is invalid with a too short email" do
@@ -50,9 +51,9 @@ RSpec.describe Admin, type: :model do
     another_admin.save
     admin.valid?
     expect(admin.errors[:email]).to include "has already been taken"
-end
+  end
 
-it "rejects invalid emails" do
+  it "rejects invalid emails" do
     invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com)
     invalid_addresses.each do |invalid_address|
@@ -64,12 +65,12 @@ it "rejects invalid emails" do
   end
 
   it "accepts valid emails" do
-  valid_addresses = %w(user@example.com USER@foo.COM A_U-ER@foo.bar.org
-                       first.last@foo.jp alice+bob@baz.cn)
-  valid_addresses.each do |valid_address|
-    admin.email = valid_address
-    admin.valid?
-    expect(admin).to be_valid, "#{valid_address.inspect} should be valid"
+    valid_addresses = %w(user@example.com USER@foo.COM A_U-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn)
+    valid_addresses.each do |valid_address|
+      admin.email = valid_address
+      admin.valid?
+      expect(admin).to be_valid, "#{valid_address.inspect} should be valid"
+    end
   end
-end
 end
