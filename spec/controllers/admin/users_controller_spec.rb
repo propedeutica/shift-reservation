e8a7491727f103d0b4879ead4e40463e8968b2ad
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-23 10:22:40 +0200

    minor fixes

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
-
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -15,5 +14,4 @@ RSpec.describe Admin::UsersController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
