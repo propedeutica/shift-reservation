ab957dbebbe5cc40e03de334d5b394460b19bcbe
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-26 14:55:59 +0200

    Generated model with type

---
+++ app/models/offspring.rb
@@ -0,0 +1,2 @@
+class Offspring < ApplicationRecord
+end
---
+++ spec/factories/offsprings.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :offspring do
+    first_name "MyString"
+    last_name "MyString"
+    type ""
+  end
+end
