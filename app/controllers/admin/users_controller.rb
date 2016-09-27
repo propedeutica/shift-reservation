c4618ca19eb3c491dd838eb2f0cf1e5489c5529b
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-27 21:57:59 +0200

    adding admin delete and update to users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,4 +10,39 @@ class Admin::UsersController < Admin::AdminIdentifiedController
   def edit
     @user = User.find_by_id(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    admin_password = params[:user][:current_password]
+    params[:user].delete :current_password
+    params[:user].delete(:password) if params[:user][:password].blank?
+    params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+    if !current_admin&.valid_password? admin_password
+      flash[:danger] = (t ".admin_password_needed")
+      render 'edit'
+    elsif @user.update_attributes(users_params)
+      flash[:success] = (t ".user_updated", user: @user.email)
+      redirect_to admin_user_path(@user)
+    else
+      flash[:danger] = (t ".user_not_updated", user: @user.email)
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params["id"])
+    if @user&.destroy
+      flash[:success] = (t ".user_deleted", user: @user.email)
+      redirect_to admin_users_path
+    else
+      flash[:danger] = (t ".user_delete_error", user: @user.email)
+      render 'edit'
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation)
+  end
 end
