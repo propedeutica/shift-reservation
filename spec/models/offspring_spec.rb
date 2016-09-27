f0154e7d7f75ef4c68bd29709584c42856eafafb
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-27 18:43:18 +0200

    Editing specs 2

--- spec/models/offspring_spec.rb
+++ spec/models/offspring_spec.rb
@@ -1,14 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Offspring, type: :model do
-  it "should create a new instance given valid attributes" do
-    FactoryGirl.create(:offspring)
-  end
+  describe "validating attribute first_name " do
+    it "should be required" do
+      blank = Factory.build(:offspring, first_name: "")
+      blank.should_not be_valid
+      blank.errors[:first_name].should include("can't be blank")
+      blank.name = "Foo"
+      blank.should be_valid
+    end
 
-  it "should initialize successfully as an instance of the described class" do
-    expect(subject).to be_a_kind_of described_class
-  end
+    it "should be longer than 1 character" do
+      too_short = Factory.build(:offspring, first_name: 'a')
+      too_short.should_not be_valid
+      too_short.errors[:first_name].should include("is too short (minimum is 2 characters)")
+      too_short.name = 'aa'
+      too_short.should be_valid
+    end
 
+    it "should be shorter than 101 characters" do
+      too_long = Factory.build(:offspring, first_name: 'a' * 101)
+      too_long.should_not be_valid
+      too_long.errors[:first_name].should include("is too long (maximum is 100 characters)")
+      too_long.name = 'a' * 100
+      too_long.should be_valid
+    end
+  end
+  describe "validating attribute last_name " do
+    it "should be required" do
+      blank = Factory.build(:offspring, last_name: "")
+      blank.should_not be_valid
+      blank.errors[:last_name].should include("can't be blank")
+      blank.name = "Foo"
+      blank.should be_valid
+    end
+
+    it "should be longer than 1 character" do
+      too_short = Factory.build(:offspring, last_name: 'a')
+      too_short.should_not be_valid
+      too_short.errors[:last_name].should include("is too short (minimum is 2 characters)")
+      too_short.name = 'aa'
+      too_short.should be_valid
+    end
+
+    it "should be shorter than 101 characters" do
+      too_long = Factory.build(:offspring, last_name: 'a' * 101)
+      too_long.should_not be_valid
+      too_long.errors[:last_name].should include("is too long (maximum is 100 characters)")
+      too_long.name = 'a' * 100
+      too_long.should be_valid
+    end
+  end
   pending("should identify its parent class")
   pending("should not exist without parent")
   pending("subclass must be able to identify with is parent class")
@@ -19,61 +61,4 @@ RSpec.describe Offspring, type: :model do
   pending("has all attributes validated")
   pending("courses are defined with an enum")
   pending("has a max and min age definde in config yaml file")
-end
-
-describe "validating attribute first_name " do
-  it "should be required" do
-    blank = Factory.build(:offspring, first_name: "")
-    blank.should_not be_valid
-    blank.errors[:first_name].should include("can't be blank")
-
-    blank.name = "Foo"
-    blank.should be_valid
-  end
-
-  it "should be longer than 1 character" do
-    too_short = Factory.build(:offspring, first_name: 'a')
-    too_short.should_not be_valid
-    too_short.errors[:first_name].should include("is too short (minimum is 2 characters)")
-
-    too_short.name = 'aa'
-    too_short.should be_valid
-  end
-
-  it "should be shorter than 101 characters" do
-    too_long = Factory.build(:offspring, first_name: 'a' * 101)
-    too_long.should_not be_valid
-    too_long.errors[:first_name].should include("is too long (maximum is 100 characters)")
-
-    too_long.name = 'a' * 100
-    too_long.should be_valid
-  end
-end
-describe "validating attribute last_name " do
-  it "should be required" do
-    blank = Factory.build(:offspring, last_name: "")
-    blank.should_not be_valid
-    blank.errors[:last_name].should include("can't be blank")
-
-    blank.name = "Foo"
-    blank.should be_valid
-  end
-
-  it "should be longer than 1 character" do
-    too_short = Factory.build(:offspring, last_name: 'a')
-    too_short.should_not be_valid
-    too_short.errors[:last_name].should include("is too short (minimum is 2 characters)")
-
-    too_short.name = 'aa'
-    too_short.should be_valid
-  end
-
-  it "should be shorter than 101 characters" do
-    too_long = Factory.build(:offspring, last_name: 'a' * 101)
-    too_long.should_not be_valid
-    too_long.errors[:last_name].should include("is too long (maximum is 100 characters)")
-
-    too_long.name = 'a' * 100
-    too_long.should be_valid
-  end
 end
