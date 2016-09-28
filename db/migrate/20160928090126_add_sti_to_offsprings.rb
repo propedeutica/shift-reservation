813b582d1840b52e14b0d95d70cf45d6180d63fc
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-28 13:54:07 +0200

    Tiny fix in migration file

--- db/migrate/20160928090126_add_sti_to_offsprings.rb
+++ db/migrate/20160928090126_add_sti_to_offsprings.rb
@@ -1,6 +1,6 @@
 class AddStiToOffsprings < ActiveRecord::Migration[5.0]
   def change
-    add_column :offsprings, :grade, :intger
+    add_column :offsprings, :grade, :integer
     add_column :offsprings, :age, :integer
   end
 end
