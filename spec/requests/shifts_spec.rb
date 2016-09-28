04fbef129ed45627e50e5a1ae9a0b9af387837ca
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-28 14:03:28 +0200

    new tests for shifts requests

--- spec/requests/shifts_spec.rb
+++ spec/requests/shifts_spec.rb
@@ -21,6 +21,15 @@ RSpec.describe "Shifts", type: :request do
       login_as(admin, scope: :admin)
       shift
       expect { get admin_shifts_destroy_all_path }.to change(Shift, :count).from(1).to(0)
+      expect(flash[:success]).to eq I18n.t('admin.shifts.destroy_all.shift_destroy_all')
+    end
+
+    it "shows flash when errors in #destroy all" do
+      expect(Shift).to receive(:destroy_all).and_return(false)
+      shift
+      login_as(admin, scope: :admin)
+      expect { get admin_shifts_destroy_all_path }.to_not change(Shift, :count)
+      expect(flash[:alert]).to eq I18n.t('admin.shifts.destroy_all.shift_destroy_all_error')
     end
 
     it "returns if there are seats available" do
@@ -33,6 +42,7 @@ RSpec.describe "Shifts", type: :request do
       login_as(admin, scope: :admin)
       get admin_shift_path(bad_shift)
       expect(response).to redirect_to admin_rooms_path
+      expect(flash[:alert]).to eq I18n.t('admin.shifts.show.shift_not_found')
     end
 
     it "#edit should show the edit template for shift" do
@@ -49,6 +59,7 @@ RSpec.describe "Shifts", type: :request do
       expect(response).to redirect_to admin_shift_path(shift.to_param)
       expect(controller.params[:id]).to eq(shift.to_param)
       expect(controller.params[:action]).to eq("update")
+      expect(flash[:success]).to eq I18n.t('admin.shifts.update.shift_updated')
     end
 
     it "#new" do
@@ -63,12 +74,31 @@ RSpec.describe "Shifts", type: :request do
       login_as(admin, scope: :admin)
       expect { post admin_room_shifts_path(room), params: { shift: FactoryGirl.attributes_for(:shift, room: room) } }
         .to change(Shift, :count).by(1)
+      expect(flash[:success]).to eq I18n.t("admin.shifts.create.shift_added", shift: Shift.last.id)
+    end
+
+    it "does not #update a shift with errors" do
+      login_as(admin, scope: :admin)
+      patch "/admin/shifts/#{shift.id}", params: { id: shift.to_param, shift: { day_of_week: 11 }}
+      expect(response).to have_http_status(200)
+      expect(controller.params[:id]).to eq(shift.to_param)
+      expect(controller.params[:action]).to eq("update")
+      expect(flash[:alert]).to eq I18n.t('admin.shifts.update.shift_not_updated')
     end
 
     it "deletes a shift" do
       login_as(admin, scope: :admin)
       shift
       expect { delete admin_shift_path(shift) }.to change(Shift, :count).by(-1)
+      expect(flash[:success]).to eq I18n.t("admin.shifts.destroy.shift_deleted", shift: shift.id)
+    end
+
+    it "can't delete a shift that doesn't exist" do
+      login_as(admin, scope: :admin)
+      shift
+      expect { delete admin_shift_path(100_000) }.to_not change(Shift, :count)
+      expect(response).to redirect_to admin_rooms_path
+      expect(flash[:alert]).to eq I18n.t("admin.shifts.destroy.shift_not_deleted")
     end
 
     pending "does not create a shift with wrong strong_parameters" do
@@ -77,6 +107,7 @@ RSpec.describe "Shifts", type: :request do
       myshift = spy(:create)
       post admin_room_shifts_path(room), params: { shift: FactoryGirl.attributes_for(:shift, room: room) }
       expect(myshift).to receive(:create).with(an_instance_of(Shift))
+      expect(flash[:success]).to eq I18n.t("admin.shifts.create.shift_added", shift: shift.id)
     end
   end
 
@@ -89,17 +120,20 @@ RSpec.describe "Shifts", type: :request do
       Warden.test_reset!
     end
 
-    it "error GET /shift" do
+    it "error GET /admin/shift" do
       login_as(user, scope: :user)
       get admin_shift_path(shift)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "destroy ALL" do
       login_as(user, scope: :user)
       get admin_shifts_destroy_all_path
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
+
     it "returns if there are seats available" do
       login_as(user, scope: :user)
       expect(shift.sites_available?).to be_truthy
@@ -110,17 +144,21 @@ RSpec.describe "Shifts", type: :request do
       login_as(user, scope: :user)
       get admin_shift_path(bad_shift)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "#edit should show the edit template for shift" do
       login_as(user, scope: :user)
       get edit_admin_shift_path(shift)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
-    it "#update should show the edit template for shift" do
+
+    it "#update should not show the edit template for shift" do
       login_as(user, scope: :user)
       patch "/admin/shifts/#{shift.id}", params: { id: shift.to_param, shift: { day_of_week: 2 }}
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "does not create a shift" do
@@ -128,6 +166,7 @@ RSpec.describe "Shifts", type: :request do
       expect { post admin_room_shifts_path(room), params: { shift: FactoryGirl.attributes_for(:shift, room: room) } }
         .not_to change(Shift, :count)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "does not delete a shift" do
@@ -135,6 +174,7 @@ RSpec.describe "Shifts", type: :request do
       shift
       expect { delete admin_shift_path(shift) }.not_to change(Shift, :count)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
   end
   context "when not authenticated" do
@@ -148,11 +188,13 @@ RSpec.describe "Shifts", type: :request do
     it "error GET /shift" do
       get admin_shift_path(shift)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "destroy ALL" do
       get admin_shifts_destroy_all_path
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
     it "returns if there are seats available" do
       expect(shift.sites_available?).to be_truthy
@@ -162,27 +204,32 @@ RSpec.describe "Shifts", type: :request do
       bad_shift = FactoryGirl.build_stubbed(:shift)
       get admin_shift_path(bad_shift)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "#edit should show the edit template for shift" do
       get edit_admin_shift_path(shift)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
     it "#update should show the edit template for shift" do
       patch "/admin/shifts/#{shift.id}", params: { id: shift.to_param, shift: { day_of_week: 2 }}
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "does not create a shift" do
       expect { post admin_room_shifts_path(room), params: { shift: FactoryGirl.attributes_for(:shift, room: room) } }
         .not_to change(Shift, :count)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
 
     it "does not delete a shift" do
       shift
       expect { delete admin_shift_path(shift) }.not_to change(Shift, :count)
       expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
     end
   end
 end
