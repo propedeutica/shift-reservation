9692688ff261af61873ad5ab4472f7a850c83f61
Sergio Ocon <sergio.ocon@redhat.com>
2016-09-29 09:45:43 +0200

    room tests and deleting unused helpers

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -11,30 +11,38 @@ class Admin::RoomsController < Admin::AdminIdentifiedController
 
   def edit
     @room = Room.find_by_id(params[:id])
+    if @room.nil?
+      flash[:alert] = (t ".room_not_found")
+      redirect_to admin_rooms_path
+    end
   end
 
   def create
     @room = Room.new(rooms_params)
     if @room.save
-      flash[:success] = (t 'application.models.room.room_added').capitalize
+      flash[:success] = (t '.room_added')
       redirect_to admin_room_path @room
     else
-      flash[:error] = (t 'application.models.room.room_not_added').capitalize
+      flash[:alert] = (t '.room_not_added')
       render 'new'
     end
   end
 
   def show
     @room = Room.find_by_id(params[:id])
+    if @room.nil?
+      flash[:alert] = (t ".room_not_found")
+      redirect_to admin_rooms_path
+    end
   end
 
   def update
     @room = Room.find_by_id(params[:id])
-    if @room.update_attributes(rooms_params)
-      flash[:success] = (t "application.models.room.room_updated").capitalize
+    if @room&.update_attributes(rooms_params)
+      flash[:success] = (t ".room_updated")
       redirect_to admin_room_path @room
     else
-      flash[:danger] = (t "application.models.room.room_not_updated").capitalize
+      flash[:alert] = (t ".room_not_updated")
       render 'edit'
     end
   end
@@ -42,15 +50,19 @@ class Admin::RoomsController < Admin::AdminIdentifiedController
   def destroy
     @room = Room.find_by_id(params["id"])
     if @room&.destroy
-      flash[:success] = (t "application.models.room.room_deleted").capitalize
+      flash[:success] = (t ".room_deleted", room: @room.name)
     else
-      flash[:danger] = (t "application.models.room.room_delete_error").capitalize
+      flash[:alert] = (t ".room_not_deleted")
     end
     redirect_to admin_rooms_path
   end
 
   def destroy_all
-    Room.destroy_all
+    if Room.destroy_all
+      flash[:success] = (t ".rooms_destroy_all")
+    else
+      flash[:alert] = (t ".rooms_destroy_all_error")
+    end
     redirect_to admin_dashboard_path
   end
 
--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -2,41 +2,333 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe "Rooms", type: :request do
-  context "when admin authenticated" do
-    describe "GET /rooms" do
-      let!(:room) { FactoryGirl.create(:room) }
-      let(:admin) { FactoryGirl.create(:admin) }
+  context "when authenticated as admin" do
+    let!(:room) { FactoryGirl.create(:room) }
+    let(:admin) { FactoryGirl.create(:admin) }
 
-      after(:each) do
-        Warden.test_reset!
-      end
+    after(:each) do
+      Warden.test_reset!
+    end
 
-      it "shows rooms index" do
-        login_as(admin, scope: :admin)
-        get admin_rooms_path
-        expect(response).to have_http_status(200)
-      end
-      it "GET room #id" do
-        login_as(admin, scope: :admin)
-        get admin_room_path(room)
-        expect(response).to have_http_status(200)
-      end
-      it "DESTROY ALL rooms" do
-        login_as(admin, scope: :admin)
-        get admin_dashboard_path
-        get destroy_all_admin_rooms_path
-        expect(Room.count).to be(0)
-      end
+    it "shows rooms index" do
+      login_as(admin, scope: :admin)
+      room
+      get admin_rooms_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(room.name)
+    end
+
+    it "can destroy all" do
+      login_as(admin, scope: :admin)
+      room
+      expect { get destroy_all_admin_rooms_path }.to change(Room, :count).from(1).to(0)
+      expect(flash[:success]).to eq I18n.t('admin.rooms.destroy_all.rooms_destroy_all')
+    end
+
+    it "shows flash when errors in #destroy_all" do
+      expect(Room).to receive(:destroy_all).and_return(false)
+      room
+      login_as(admin, scope: :admin)
+      expect { get destroy_all_admin_rooms_path }.to_not change(Room, :count)
+      expect(flash[:alert]).to eq I18n.t('admin.rooms.destroy_all.rooms_destroy_all_error')
+    end
+
+    it "SHOW room #id" do
+      login_as(admin, scope: :admin)
+      room
+      get admin_room_path(room)
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(room.name)
+    end
+
+    it "can't show wrong #id" do
+      bad_room = FactoryGirl.build_stubbed(:room)
+      login_as(admin, scope: :admin)
+      get admin_room_path(bad_room)
+      expect(response).to redirect_to admin_rooms_path
+      expect(flash[:alert]).to eq I18n.t('admin.rooms.show.room_not_found')
+    end
+
+    it "#edit should show the edit template for #room" do
+      login_as(admin, scope: :admin)
+      get edit_admin_room_path(room)
+      expect(response).to have_http_status(200)
+      expect(controller.params[:id]).to eq(room.to_param)
+      expect(controller.params[:action]).to eq("edit")
+    end
+
+    it "#edit shows flash when room does not exist" do
+      login_as(admin, scope: :admin)
+      get edit_admin_room_path(100)
+      expect(response).to redirect_to admin_rooms_path
+      expect(controller.params[:action]).to eq("edit")
+      expect(flash[:alert]).to eq I18n.t('admin.rooms.edit.room_not_found')
+    end
+
+    it "#update should update the room via post" do
+      login_as(admin, scope: :admin)
+      room
+      patch "/admin/rooms/#{room.id}", params: { id: room.to_param, room: FactoryGirl.attributes_for(:room) }
+      expect(response).to redirect_to admin_room_path(room.to_param)
+      expect(controller.params[:id]).to eq(room.to_param)
+      expect(controller.params[:action]).to eq("update")
+      expect(flash[:success]).to eq I18n.t('admin.rooms.update.room_updated')
+    end
+
+    it "#update should not update the room with errors" do
+      login_as(admin, scope: :admin)
+      patch "/admin/rooms/#{room.id}", params: { id: room.to_param, room: { capacity: -1 }}
+      expect(response).to have_http_status(200)
+      expect(controller.params[:id]).to eq(room.to_param)
+      expect(controller.params[:action]).to eq("update")
+      expect(flash[:alert]).to eq I18n.t('admin.rooms.update.room_not_updated')
+    end
+
+    it "#new" do
+      login_as(admin, scope: :admin)
+      get new_admin_room_path(room)
+      expect(response).to have_http_status(200)
+      expect(controller.params[:action]).to eq('new')
+    end
+
+    it "#create" do
+      login_as(admin, scope: :admin)
+      expect { post admin_rooms_path(room), params: { room: FactoryGirl.attributes_for(:room) } }
+        .to change(Room, :count).by(1)
+      expect(flash[:success]).to eq I18n.t("admin.rooms.create.room_added", room: room.name)
+      expect(response).to redirect_to admin_room_path Room.last
+    end
+
+    it "doesn't create with errors" do
+      login_as(admin, scope: :admin)
+      myparams = { room: FactoryGirl.attributes_for(:room, capacity: -1) }
+      expect { post admin_rooms_path, params: myparams }.to_not change(Room, :count)
+      expect(flash[:alert]).to eq I18n.t("admin.rooms.create.room_not_added")
+    end
+
+    it "#delete" do
+      login_as(admin, scope: :admin)
+      room
+      expect { delete admin_room_path(room) }.to change(Room, :count).by(-1)
+      expect(flash[:success]).to eq I18n.t("admin.rooms.destroy.room_deleted", room: room.name)
+      expect(response).to redirect_to admin_rooms_path
+    end
+
+    it "can't #delete without room" do
+      login_as(admin, scope: :admin)
+      room
+      expect { delete admin_room_path(100_000) }.to_not change(Room, :count)
+      expect(response).to redirect_to admin_rooms_path
+      expect(flash[:alert]).to eq I18n.t("admin.rooms.destroy.room_not_deleted")
     end
   end
+
   context "when authenticated as user" do
-    pending "does not show rooms index"
-    pending "error GET room #id"
-    pending "error destroy all romoms"
+    let(:room) { FactoryGirl.create(:room) }
+    let(:user) { FactoryGirl.create(:user) }
+
+    after(:each) do
+      Warden.test_reset!
+    end
+
+    before(:each) do
+      login_as(user, scope: user)
+    end
+
+    it "shows rooms index" do
+      room
+      get admin_rooms_path
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "can destroy all" do
+      room
+      expect { get destroy_all_admin_rooms_path }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "shows flash when errors in #destroy_all" do
+      room
+      expect { get destroy_all_admin_rooms_path }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "SHOW room #id" do
+      room
+      get admin_room_path(room)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "can't show wrong #id" do
+      bad_room = FactoryGirl.build_stubbed(:room)
+      get admin_room_path(bad_room)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#edit should show the edit template for #room" do
+      get edit_admin_room_path(room)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#edit shows flash when room does not exist" do
+      get edit_admin_room_path(100)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#update should update the room via post" do
+      room
+      patch "/admin/rooms/#{room.id}", params: { id: room.to_param, room: FactoryGirl.attributes_for(:room) }
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#update should not update the room with errors" do
+      patch "/admin/rooms/#{room.id}", params: { id: room.to_param, room: { capacity: -1 }}
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#new" do
+      get new_admin_room_path(room)
+      expect(response).to have_http_status(401)
+    end
+
+    it "#create" do
+      room
+      expect { post admin_rooms_path, params: { room: FactoryGirl.attributes_for(:room) } }
+        .to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "doesn't create with errors" do
+      myparams = { room: FactoryGirl.attributes_for(:room, capacity: -1) }
+      expect { post admin_rooms_path, params: myparams }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#delete" do
+      room
+      expect { delete admin_room_path(room) }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "can't #delete without room" do
+      room
+      expect { delete admin_room_path(100_000) }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
   end
+
   context "when not authenticated" do
-    pending "does not show rooms index"
-    pending "error GET room #id"
-    pending "error destroy all romoms"
+    let(:room) { FactoryGirl.create(:room) }
+
+    after(:each) do
+      Warden.test_reset!
+    end
+
+    it "shows rooms index" do
+      room
+      get admin_rooms_path
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "can destroy all" do
+      room
+      expect { get destroy_all_admin_rooms_path }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "shows flash when errors in #destroy_all" do
+      room
+      expect { get destroy_all_admin_rooms_path }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "SHOW room #id" do
+      room
+      get admin_room_path(room)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "can't show wrong #id" do
+      bad_room = FactoryGirl.build_stubbed(:room)
+      get admin_room_path(bad_room)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#edit should show the edit template for #room" do
+      get edit_admin_room_path(room)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#edit shows flash when room does not exist" do
+      get edit_admin_room_path(100)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#update should update the room via post" do
+      room
+      patch "/admin/rooms/#{room.id}", params: { id: room.to_param, room: FactoryGirl.attributes_for(:room) }
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#update should not update the room with errors" do
+      patch "/admin/rooms/#{room.id}", params: { id: room.to_param, room: { capacity: -1 }}
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#new" do
+      get new_admin_room_path(room)
+      expect(response).to have_http_status(401)
+    end
+
+    it "#create" do
+      room
+      expect { post admin_rooms_path, params: { room: FactoryGirl.attributes_for(:room) } }
+        .to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "doesn't create with errors" do
+      myparams = { room: FactoryGirl.attributes_for(:room, capacity: -1) }
+      expect { post admin_rooms_path, params: myparams }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "#delete" do
+      room
+      expect { delete admin_room_path(room) }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
+
+    it "can't #delete without room" do
+      room
+      expect { delete admin_room_path(100_000) }.to_not change(Room, :count)
+      expect(response).to redirect_to(new_admin_session_path)
+      expect(flash[:alert]).to eq I18n.t "devise.failure.unauthenticated"
+    end
   end
 end
