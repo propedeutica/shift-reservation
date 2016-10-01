06ae7d9ce76addaca49e5e97f12b142c159a3974
Sergio Ocón <sergio.ocon@google.com>
2016-10-01 16:28:11 +0200

    added DatabaseCleaner config

--- tools/ci/before_script.sh
+++ tools/ci/before_script.sh
@@ -1,7 +1,10 @@
 set -v
 
 if [[ "$TEST_SUITE" == "rspec" ]]; then
-  bin/rails db:drop db:create db:migrate RAILS_ENV=test --trace
+  # bin/rails db:drop db:create db:migrate RAILS_ENV=test --trace
+  # bin/rails db:environment:set RAILS_ENV=test --trace
+  bin/rails db:environment:set RAILS_ENV=test
+  bin/rails db:migrate RAILS_ENV=test
 fi
 
 set +v
