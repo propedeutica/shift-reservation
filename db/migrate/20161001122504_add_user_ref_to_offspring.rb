0874f2f14f8fca3ba31a21b44c1b6a1fe0cd6ea2
Sergio Ocón <sergio.ocon@google.com>
2016-10-01 19:55:46 +0200

    a ver ahora

--- db/migrate/20161001122504_add_user_ref_to_offspring.rb
+++ db/migrate/20161001122504_add_user_ref_to_offspring.rb
@@ -1,5 +1,5 @@
 class AddUserRefToOffspring < ActiveRecord::Migration[5.0]
   def change
-    add_reference :offsprings, :user, foreign_key: true, null: false
+    add_reference :offsprings, :user, foreign_key: true
   end
 end
